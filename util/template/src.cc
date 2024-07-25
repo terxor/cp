685120c8a18d8798c320e7de9331ceec2640f808
terxor <39589847+terxor@users.noreply.github.com>
2024-07-26 00:55:11 +0530

    update template

--- util/template/src.cc
+++ util/template/src.cc
@@ -1,15 +1,15 @@
 // template {{{
 #include <bits/stdc++.h>
 using namespace std;
+using i64 = int64_t;
 #ifdef LOCAL
 #include <debug.hh>
 #else
 #define dbg(...) ;
 #endif
-#define long int64_t
 void solution();
 int main() {
-  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  ios_base::sync_with_stdio(0), cin.tie(0);
   cout << fixed << setprecision(11);
   return solution(), 0;
 } // }}}
